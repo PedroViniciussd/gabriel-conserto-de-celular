2b1c48108f6862716f998f0754afb039f8825d55
Pedro Vinicius <pedrovinicius.sd@gmail.com>
2026-05-22 18:51:31 -0300

    adiciona sitemap e robots

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://gabrieltechelite.vercel.app/sitemap.xml',
+  }
+}
